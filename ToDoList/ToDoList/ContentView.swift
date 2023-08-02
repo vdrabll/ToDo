97e45460f80f497ae559b550fe71b0fe9504e8b6
SweetestSoul <61381174+vdrabll@users.noreply.github.com>
2023-08-02 14:35:47 +0300

    Add base project files
    #1 issue - add linter

---
+++ ToDoList/ToDoList/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Виктория Федосова on 02.08.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Виктория Федосова on 02.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
