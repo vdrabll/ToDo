c8373cf8e5758c59815834f691e31dfa125cf26a
vdrabll <fedosova.univ@gmail.com>
2023-08-02 17:43:57 +0300

    add coredata class

---
+++ ToDoList/Task+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  Task+CoreDataProperties.swift
+//  ToDoList
+//
+//  Created by Виктория Федосова on 02.08.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoTask)
+public class ToDoTask: NSManagedObject {
+
+}
+
+extension ToDoTask {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoTask> {
+        return NSFetchRequest<ToDoTask>(entityName: "ToDoTask")
+    }
+
+    @NSManaged public var id: UUID?
+    @NSManaged public var isCheked: Bool
+    @NSManaged public var title: String?
+
+}
+
+extension ToDoTask : Identifiable {
+
+}
