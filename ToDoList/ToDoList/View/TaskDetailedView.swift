7897d3110caea8422fb9f233e762b11fdec2cbf2
vdrabll <fedosova.univ@gmail.com>
2023-08-08 18:26:48 +0300

    fix background colors in taskview

--- ToDoList/ToDoList/View/TaskDetailedView.swift
+++ ToDoList/ToDoList/View/TaskDetailedView.swift
@@ -34,17 +34,14 @@ struct TaskEditView: View {
 				.font(.system(size: 40))
 				.bold()
 			Spacer(minLength: 90)
-			
 			TextField("", text: $title)
 				.frame(width: 350.0,
-					   height: 50.0 ,
+					   height: 50.0,
 					   alignment: .center)
 				.font(.title3)
 				.background(.white)
 				.cornerRadius(10.0)
 			Spacer(minLength: 150)
-			
-			
 			Button(Constants.buttonTitle) {
 				if passedTask == nil {
 					passedTask = ToDoTask(context: managedObjectContext)
@@ -57,17 +54,13 @@ struct TaskEditView: View {
 			}
 			.fontWeight(.medium)
 			.tint(.black)
-	
 		}
 		.padding(25.0)
 		.font(.system(size: 40))
 			.buttonStyle(.borderedProminent)
 			.controlSize(.large)
-			.background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("textfield.background")/*@END_MENU_TOKEN@*/)
-			
+			.background(.clear)
 	}
-		
-	
 	func save(context: NSManagedObjectContext) {
 		do {
 			try context.save()
@@ -77,11 +70,10 @@ struct TaskEditView: View {
 	}
 }
 
-
 struct TaskEditView_Previews: PreviewProvider {
 	static var previews: some View {
 		TaskEditView(task: nil)
 			.padding()
-			.background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("textfield.background")/*@END_MENU_TOKEN@*/)
+			.background(.clear)
 	}
 }
